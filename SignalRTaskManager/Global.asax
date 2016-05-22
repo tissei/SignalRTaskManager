83b385101fa81d29288eceae2af5c6f57c6b9226
José Carlos S.A. Tissei <Jos Carlos S.A. Tissei>
2016-05-22 15:02:43 -0300

    Adicionada solution

---
+++ SignalRTaskManager/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SignalRTaskManager.MvcApplication" Language="C#" %>
---
+++ SignalRTaskManager/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SignalRTaskManager.Startup))]
+namespace SignalRTaskManager
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
