d9c8044b74fdf2c1df6fee81f8a198d5550478ba
José Carlos S.A. Tissei <Jos Carlos S.A. Tissei>
2016-05-22 17:49:03 -0300

    adicionado bower

--- SignalRTaskManager/App_Start/BundleConfig.cs
+++ SignalRTaskManager/App_Start/BundleConfig.cs
@@ -8,23 +8,15 @@ namespace SignalRTaskManager
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/assets/scripts/jquery-{version}.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/assets/scripts/jquery.validate*"));
-
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/assets/scripts/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/assets/scripts/bootstrap.js",
-                      "~/assets/scripts/respond.js"));
+            bundles.Add(new ScriptBundle("~/bundles/scripts")
+                .Include("~/assets/scripts/plugins/jQuery/dist/jquery.min.js")
+                .Include("~/assets/scripts/plugins/jquery-validation/dist/jquery.validate.min.js")
+                .Include("~/assets/scripts/plugins/bootstrap/dist/js/bootstrap.min.js")
+                .Include("~/assets/scripts/plugins/angular/angular.min.js")
+                .Include("~assets/scripts/plugins/signalr/jquery.signalR.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/assets/css/bootstrap.css",
+                      "~/assets/scripts/plugins/bootstrap/dist/css/bootstrap.min.css",
                       "~/assets/css/site.css"));
         }
     }
